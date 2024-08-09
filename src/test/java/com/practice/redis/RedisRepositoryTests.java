fd824ff2f75cd14cd1d2c40aeb0b1aefa01c1333
wonowonow <wonow_@naver.com>
2024-08-09 12:28:42 +0900

    StringRedisTemplate

--- src/test/java/com/practice/redis/RedisRepositoryTests.java
+++ src/test/java/com/practice/redis/RedisRepositoryTests.java
@@ -1,55 +1,55 @@
-package com.practice.redis;
-
-import static org.assertj.core.api.Assertions.*;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class RedisRepositoryTests {
-
-    @Autowired
-    private ItemRepository itemRepository;
-
-    @Test
-    public void createTest() {
-        Item item = Item.builder()
-//                .id(1L) // 직접 넣을 수도 있다
-                .name("keyboard")
-                .description("description")
-                .price(100000)
-                .build();
-
-        itemRepository.save(item);
-        // id = Random Long Set
-        // id 기준으로 hash table 검색 후 매핑
-    }
-
-    @Test
-    public void readOneTest() {
-        Item item = itemRepository.findById(1L)
-                .orElseThrow();
-
-        assertThat(item.getDescription()).isEqualTo("description");
-    }
-
-    @Test
-    public void updateOneTest() {
-        Item item = itemRepository.findById(1L)
-                .orElseThrow();
-
-        item.setDescription("hello");
-        Item savedItem = itemRepository.save(item);
-
-        assertThat(savedItem.getDescription()).isEqualTo("hello");
-    }
-
-    @Test
-    void deleteOneTest() {
-        itemRepository.deleteById(1L);
-
-        assertThat(itemRepository.findById(1L).isPresent()).isFalse();
-    }
-}
+//package com.practice.redis;
+//
+//import static org.assertj.core.api.Assertions.*;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class RedisRepositoryTests {
+//
+//    @Autowired
+//    private ItemRepository itemRepository;
+//
+//    @Test
+//    public void createTest() {
+//        Item item = Item.builder()
+////                .id(1L) // 직접 넣을 수도 있다
+//                .name("keyboard")
+//                .description("description")
+//                .price(100000)
+//                .build();
+//
+//        itemRepository.save(item);
+//        // id = Random Long Set
+//        // id 기준으로 hash table 검색 후 매핑
+//    }
+//
+//    @Test
+//    public void readOneTest() {
+//        Item item = itemRepository.findById(1L)
+//                .orElseThrow();
+//
+//        assertThat(item.getDescription()).isEqualTo("description");
+//    }
+//
+//    @Test
+//    public void updateOneTest() {
+//        Item item = itemRepository.findById(1L)
+//                .orElseThrow();
+//
+//        item.setDescription("hello");
+//        Item savedItem = itemRepository.save(item);
+//
+//        assertThat(savedItem.getDescription()).isEqualTo("hello");
+//    }
+//
+//    @Test
+//    void deleteOneTest() {
+//        itemRepository.deleteById(1L);
+//
+//        assertThat(itemRepository.findById(1L).isPresent()).isFalse();
+//    }
+//}
---
+++ src/test/java/com/practice/redis/RedisTemplateTests.java
@@ -0,0 +1,44 @@
+package com.practice.redis;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+public class RedisTemplateTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void stringOpsTest() {
+        // 문자열 조작을 위한 클래스
+        ValueOperations<String, String> ops
+                // 지금 RedisTemplate 에 설정 된 타입을 바탕으로
+                // Redis 문자열을 조작 할 것이다.
+                // 여기서 말하는 String 은 Redis 의 String 이 아니다.
+                // 자바에서의 타입을 말하는 것이다.
+                // 즉 레디스를 어떤 타입으로 다룰 것인지 정하는 것이다.
+                = stringRedisTemplate.opsForValue();
+
+        ops.set("simplekey", "simplevalue");
+        System.out.println(ops.get("simplekey"));
+
+        // 집합을 조작하기 위한 클래스
+        SetOperations<String, String> setOps
+                = stringRedisTemplate.opsForSet();
+
+        setOps.add("hobbies", "games");
+        setOps.add("hobbies", "science", "coding", "alcohol");
+
+        System.out.println(setOps.size("hobbies"));
+
+        // Expire 설정
+        stringRedisTemplate.expire("hobbies", 10, TimeUnit.SECONDS);
+        stringRedisTemplate.delete("simplekey");
+    }
+}
